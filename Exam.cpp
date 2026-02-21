ab1dd4e2cf9045dbbd93265cc9d8b6109aa0d83f
Elvis-KKD <mojo.dar.carly@gmail.com>
2026-02-20 19:33:56 -1200

    My first comment

---
+++ Exam.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Cat!, Come and start with them and also need the light of the God.........."  << endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyFirst_Push
+My first repository!!!!
