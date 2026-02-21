8a766c8f0a4fcc44ab5b665a173d128f82846d99
Elvis-KKD <mojo.dar.carly@gmail.com>
2026-02-21 04:48:22 -1200

    multiple section of code

--- Exam.cpp
+++ Exam.cpp
@@ -5,3 +5,13 @@ int main() {
     cout << "Hello Cat!, Come and okay start with them and also need the light of the God.........."  << endl;
     return 0;
 }
+
+int main() {
+    cout << "Hello Cat!, Come and okay start with them and also need the light of the God.........."  << endl;
+    return 0;
+}
+
+int main() {
+    cout << "Hello Cat!, Come and okay start with them and also need the light of the God.........."  << endl;
+    return 0;
+}
